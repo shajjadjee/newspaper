7e86b771d3929e540348ae068b3daef3a3282c3f
shajjadjee <shajjadj2ee@gmail.com>
2020-11-23 13:06:22 +0600

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DasboardComponent } from './dasboard/dasboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NewspostComponent } from './newspost/newspost.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'signup', component: SignupComponent
+  },
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    component: DasboardComponent,
+    children: [
+      {
+        path: 'post',
+        component: NewspostComponent
+      },
+      {
+        path: 'user',
+        component: UserComponent
+      },
+      // {
+      //   path: '**',
+      //   component: DasboardComponent
+      // }
+    ],
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { NewspostComponent } from './newspost/newspost.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DasboardComponent } from './dasboard/dasboard.component';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserComponent,
+    NewspostComponent,
+    LoginComponent,
+    SignupComponent,
+    DasboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
