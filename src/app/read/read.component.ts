2b31f84d33297b359ef6bede5a1ef08e7b2435be
Shajjad <shajjadjee@users.noreply.github.com>
2020-12-09 07:27:29 +0600

    comm

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Comments } from '../model/comment.model';
 import { Post } from '../model/post.model';
+import { CommentsService } from '../service/comments.service';
 import { PostService } from '../service/post.service';
 
 @Component({
@@ -11,34 +14,58 @@ import { PostService } from '../service/post.service';
 })
 export class ReadComponent implements OnInit {
 
-  postID: any; 
-  postData: any; 
-  post: Post= new Post(0, '', '', '','slide1.jpg','','','');
+  postID: any;
+  postData: any;
+  post: Post = new Post(0, '', '', '', 'slide1.jpg', '', '', '');
   postList: Post[];
-  constructor(private ps: PostService, private http: HttpClient, private router: Router, private actRoute: ActivatedRoute) { }
+  comments: Comments = new Comments(0, 0, '', '');
+  commentList: Comments[];
+  username: string;
+  constructor(private cs: CommentsService, private ps: PostService, private http: HttpClient, private router: Router, private actRoute: ActivatedRoute, private toastr: ToastrService) { }
 
   ngOnInit(): void {
-    
-    this.postID= this.actRoute.snapshot.params['id'];
+
+    this.postID = this.actRoute.snapshot.params['id'];
     this.loadPostDetails(this.postID);
     this.getPostList();
+    this.username = this.readUsername('auth_username');
   }
   // loadPostDetails(postID){
   //   this.ps.getPostDetails(postID).subscribe(data=>)
   // }
-  
-  loadPostDetails(postID){
+
+  loadPostDetails(postID) {
     this.ps.getPostDetails(postID).subscribe(post => {
       this.postData = post;
     });
   }
-  navigation(link){
+  navigation(link) {
     this.router.navigate([link]);
   }
-  getPostList(){
+  saveComment() {
+    this.http.post<Comments>('http://localhost:8080/api/comment/save', this.comments)
+      .subscribe(data => {
+        if (data != null) {
+          this.toastr.success('success', 'Save success!');
+        }
+        this.router.navigate(['/']);
+      });
+  }
+  getCommentList() {
+    this.cs.getCommentList().subscribe(data => {
+      this.commentList = data;
+    })
+  }
+  getPostList() {
     this.ps.getPostList().subscribe(data => {
-     this.postList = data;      
-   })
+      this.postList = data;
+    })
+  }
+  readUsername(key: string): string {
+    return localStorage.getItem(key);
+  }
+  proccessname() {
+    return this.username;
   }
 
 }
